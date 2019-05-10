40b281eb65f9916e9e398bc7e22bc2dcf5e15e41
eldelcesar <cesarjr.espinosa@gmail.com>
2019-05-10 18:39:26 -0500

    Implemented basic task component and task view

---
+++ front/src/components/TaskComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="task">
+        <h3>Task 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet risus dolor.</p>
+    </div> 
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+export default class TaskComponent extends Vue {
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+.task {
+    width: 300px;
+    height: 300px;
+    border: 1px solid lightgray;
+}
+</style>
---
+++ front/src/views/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tasks">
+    <h1>Tasks</h1>
+    <TaskComponent/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TaskComponent from '@/components/TaskComponent.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+      TaskComponent,
+  },
+})
+
+export default class Tasks extends Vue {
+
+}
+</script>
