fd61f55184fb408ffaf8cb16ad9eff98dbbbe03e
eldelcesar <cesarjr.espinosa@gmail.com>
2019-05-12 13:54:00 -0500

    Updated task client verified by the service owner

--- front/src/clients/TasksClient.ts
+++ front/src/clients/TasksClient.ts
@@ -1,19 +1,22 @@
 export default class TaskClient {
 
     async GetTasks(userId: string){
+        // Receives Array of Tasks
         var res = await fetch(`api/tasks/${userId}`, {
             method: "GET"
         });
 
         if (res.ok) {
             var json = await res.json();
-            return json as TaskVM;
+            return json as TaskVM[];
         }
-
+        
+        console.log(`ERROR: ${res}`);
         return null;
     }
 
     async CreateTask(newTask: TaskVM) {
+        // Do not send Task ID
         var res = await fetch(`api/tasks/${newTask.userId}`, {
             method: "POST",
             headers: {
@@ -27,11 +30,13 @@ export default class TaskClient {
             return json as TaskVM;
         }
 
+        console.log(`ERROR: ${res}`);
         return null;
     }
 
     async UpdateTask(task: TaskVM) {
-        var res = await fetch(`api/tasks`, {
+        // Do not send Task ID and User ID
+        var res = await fetch(`api/tasks/${task.userId}/${task.taskId}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
@@ -44,20 +49,23 @@ export default class TaskClient {
             return json as TaskVM;
         }
 
+        console.log(`ERROR: ${res}`);
         return null;
     }
 
     async DeleteTask(id: string, userId: string) {
+
         var res = await fetch(`api/tasks/${userId}/${id}`, {
             method: "DELETE"
         });
 
         if (res.ok) {
-            var json = res.json();
-            return json;
+            var json = await res.json();
+            return json as TaskVM;
         }
 
-        return null
+        console.log(`ERROR: ${res}`);
+        return null;
     }  
 
 }
