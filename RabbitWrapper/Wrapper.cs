5615c599bfa0f512ddfa3b097f6f9e40eaadf5df
Miguel Pérez García <miguelpg_95@hotmail.com>
2019-05-13 07:58:38 -0500

    Adding wrapper

---
+++ RabbitWrapper/Wrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace RabbitWrapper
+{
+    public class Wrapper
+    {
+        private readonly string _rabbitUrl;
+        private IModel _connection;
+        public Wrapper(string url) => _rabbitUrl = url;
+
+        public void Start()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(_rabbitUrl);
+            var connection = factory.CreateConnection();
+            _connection = connection.CreateModel();
+        }
+
+
+    }
+}
